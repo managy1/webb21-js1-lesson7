cbc8ed2280433391a412061dbfc9a752f3f967ac
Madelene Nagy <madelene.nagy@gmail.com>
2021-09-27 09:55:34 +0200

    Add info

--- grouptask.js
+++ grouptask.js
@@ -1,1 +1,8 @@
 console.log("Hej")
+const onBuyButtonClick = () => {
+    var select = document.getElementById('coffeeType');
+        var value = select.options[select.selectedIndex].value;
+        const input = document.getElementById("numberOfCoffees").input
+        
+        console.log(value, input); 
+}
