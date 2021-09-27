fa08b1a8114de09984b30188a0c1913eeffcfaa1
Madelene Nagy <madelene.nagy@gmail.com>
2021-09-27 11:27:59 +0200

    Add functions

--- grouptask.js
+++ grouptask.js
@@ -1,8 +1,29 @@
 console.log("Hej")
+const typesOfCoffee = [
+    {
+        type: "coffee", 
+        cost: 20,
+    },
+    {
+        type: "cappuccino",
+        cost: 30,
+    },
+    {
+        type: "latte",
+        cost: 40,
+    }
+]
 const onBuyButtonClick = () => {
     var select = document.getElementById('coffeeType');
-        var value = select.options[select.selectedIndex].value;
-        const input = document.getElementById("numberOfCoffees").input
+        var coffeeType = select.options[select.selectedIndex].value;
+        const quantity = document.getElementById("numberOfCoffees").value
         
-        console.log(value, input); 
+        coffeePrice(coffeeType, quantity)
+}
+const coffeePrice = (coffeeType, quantity) => {
+    for( let i = 0; i < typesOfCoffee.length; i++ ){
+        if(typesOfCoffee[i].type === coffeeType){
+            console.log(typesOfCoffee[i].cost)
+        } 
+    }
 }
